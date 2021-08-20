1d213fb88bb93d9ac5ec2702cc263621e9b40c7c
Tahmid Islam <tahmid231@gmail.com>
2021-08-21 02:05:15 +0600

    Done with every feature

--- js/app.js
+++ js/app.js
@@ -1,62 +1,30 @@
-const memoryPrice = document.getElementById("memory-price");
-const storagePrice = document.getElementById("storage-price");
-const deliveryPrice = document.getElementById("delivery-price");
-const totalPrice = document.getElementById("total-price");
-const grandTotalPrice = document.getElementById("grand-total");
-const promoInput = document.getElementById("promo-code");
-
-//functions
-//for get the Id
-// function getId(Id) {
-//   const input = document.getElementById(Id);
-//   return input;
-// }
-
-document.getElementById("memory1").addEventListener("click", function () {
-  memoryPrice.innerText = "0";
-  updateTotal();
-});
-document.getElementById("memory2").addEventListener("click", function () {
-  memoryPrice.innerText = "180";
-  updateTotal();
-});
-document.getElementById("storage1").addEventListener("click", function () {
-  storagePrice.innerText = "0";
-  updateTotal();
-});
-document.getElementById("storage2").addEventListener("click", function () {
-  storagePrice.innerText = "100";
-  updateTotal();
-});
-document.getElementById("storage3").addEventListener("click", function () {
-  storagePrice.innerText = "180";
-  updateTotal();
-});
-
-document.getElementById("delivery1").addEventListener("click", function () {
-  deliveryPrice.innerText = "0";
-  updateTotal();
-});
-document.getElementById("delivery2").addEventListener("click", function () {
-  deliveryPrice.innerText = "20";
-  updateTotal();
-});
-
+// functions
+// for get the id
+function getId(Id) {
+  return document.getElementById(Id);
+}
+// update total price
 function updateTotal() {
-  let memory = parseInt(memoryPrice.innerText);
-  let storage = parseInt(storagePrice.innerText);
-  let delivery = parseInt(deliveryPrice.innerText);
+  let memory = parseInt(getId("memory-price").innerText);
+  let storage = parseInt(getId("storage-price").innerText);
+  let delivery = parseInt(getId("delivery-price").innerText);
   let total = 1299 + memory + storage + delivery;
-  totalPrice.innerText = total;
-  grandTotalPrice.innerText = total;
+  getId("total-price").innerText = total;
+  getId("grand-total-price").innerText = total;
+}
+
+// on click function for every button
+function update(Id, price) {
+  document.getElementById(Id + "-price").innerText = price;
+  updateTotal();
 }
 
 document.getElementById("promo-button").addEventListener("click", function () {
-  let price = parseFloat(totalPrice.innerText);
+  let price = parseFloat(getId("total-price").innerText);
   let discount = (20 / 100) * price;
   console.log(discount);
-  if (promoInput.value.toLowerCase() == "stevekaku") {
-    grandTotalPrice.innerText = price - discount;
-    promoInput.value = "";
+  if (getId("promo-code").value.toLowerCase() == "stevekaku") {
+    getId("grand-total-price").innerText = price - discount;
+    getId("promo-code").value = "";
   }
 });
